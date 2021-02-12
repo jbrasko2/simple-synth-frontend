c0a2e704419766a8887256de3d56ffb5b71e668e
jbrasko <jbrasko0508@gmail.com>
2021-02-12 11:58:35 -0600

    add save button

--- index.js
+++ index.js
@@ -26,34 +26,44 @@ let attack = new Nexus.Slider('#attack', {
     'min': 0.01,
     'max': 2
 })
-
-attack.on('change', function(v) {
-    synth.envelope.set({'attack': v})
-})
-
 let decay = new Nexus.Slider('#decay', {
     'min': 0,
     'max': 2
 })
-
-decay.on('change', function(v) {
-    synth.envelope.set({'decay': v})
-})
-
 let sustain = new Nexus.Slider('#sustain', {
     'min': 0,
     'max': 1
 })
-
-sustain.on('change', function(v) {
-    synth.envelope.set({'sustain': v})
-})
-
 let release = new Nexus.Slider('#release', {
     'min': 0,
     'max': 5
 })
 
+attack.on('change', function(v) {
+    synth.envelope.set({'attack': v}),
+    synth.filterEnvelope.set({'attack': v})
+})
+
+decay.on('change', function(v) {
+    synth.envelope.set({'decay': v})
+    synth.filterEnvelope.set({'decay': v})
+})
+
+sustain.on('change', function(v) {
+    synth.envelope.set({'sustain': v})
+    synth.filterEnvelope.set({'sustain': v})
+})
+
 release.on('change', function(v) {
     synth.envelope.set({'release': v})
+    synth.filterEnvelope.set({'release': v})
+})
+
+const saveButton = document.getElementById('save')
+
+saveButton.addEventListener('click', () => {
+    console.log(synth.envelope.attack)
+    console.log(synth.envelope.decay)
+    console.log(synth.envelope.sustain)
+    console.log(synth.envelope.release)
 })
