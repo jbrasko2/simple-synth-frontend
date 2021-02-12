127da847b40a53a5b4ebd5c37aa3b95e10bedcc7
jbrasko <jbrasko0508@gmail.com>
2021-02-12 16:18:35 -0600

    add reverb, delay, oscillator

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
+const reverb = new Tone.Reverb({"wet": 0.25}).toDestination()
+const delay = new Tone.PingPongDelay({"wet": 0})
+
 let synth = new Tone.MonoSynth({
     "filterEnvelope" : {
         "attack": 0.1,
-        "decay": 0.1,
-        "sustain": 0.2,
-        "release": 0.9
+        "decay": 1.5,
+        "sustain": 0.5,
+        "release": 3
     }
-}).toDestination();
+}).chain(delay, reverb)
+
+Tone.Destination.volume.rampTo(-5, 0.9)
+
 
 const keys = document.querySelectorAll(".key")
 
@@ -21,6 +27,19 @@ keys.forEach((key) => {
     })
 })
 
+let waveTypeButton = new Nexus.RadioButton('#waveTypeButton', {
+    'numberOfButtons': 4
+})
+
+let reverbDial = new Nexus.Dial('#reverbDial', {
+    'min': 0,
+    'max': 1
+})
+
+let delayDial = new Nexus.Dial('#delayDial', {
+    'min': 0,
+    'max': 0.5
+})
 
 let attack = new Nexus.Slider('#attack', {
     'min': 0.01,
@@ -37,6 +56,21 @@ let sustain = new Nexus.Slider('#sustain', {
 let release = new Nexus.Slider('#release', {
     'min': 0,
     'max': 5
+})
+
+let oscilloscope = new Nexus.Oscilloscope('#oscilloscope')
+oscilloscope.connect(Tone.Master)
+
+waveTypeButton.on('change', function(v) {
+    if (v === 0) {
+        synth.oscillator.set({'type': 'sine'})
+    } else if (v === 1) {
+        synth.oscillator.set({'type': 'square'})
+    } else if (v === 2) {
+        synth.oscillator.set({'type': 'triangle'})
+    } else {
+        synth.oscillator.set({'type': 'sawtooth'})
+    }
 })
 
 attack.on('change', function(v) {
@@ -59,11 +93,37 @@ release.on('change', function(v) {
     synth.filterEnvelope.set({'release': v})
 })
 
+reverbDial.on('change', function(v) {
+    reverb.set({'wet': v})
+})
+
+delayDial.on('change', function(v) {
+    delay.set({'wet': v})
+})
+
 const saveButton = document.getElementById('save')
 
 saveButton.addEventListener('click', () => {
+    console.log(synth.oscillator.type)
     console.log(synth.envelope.attack)
     console.log(synth.envelope.decay)
     console.log(synth.envelope.sustain)
     console.log(synth.envelope.release)
+    console.log(reverb.wet.value)
+    console.log(delay.wet.value)
+})
+
+const loadButton = document.getElementById('load')
+
+loadButton.addEventListener('click', () => {
+    attack.value = 0
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+    attack.value = 0.1
+    decay.value = 1.5
+    sustain.value = 0.5
+    release.value = 3
+    waveTypeButton.select(3)
+    reverbDial.value = 0.25
 })
